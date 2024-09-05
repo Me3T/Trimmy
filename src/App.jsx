86f5504ff4e882b93fd3a6a0ef022349c700d866
Me3T <rai.manmeet1998@gmail.com>
2024-09-05 16:10:21 +0530

    URL Shortner

---
+++ src/App.jsx
@@ -0,0 +1,56 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./App.css";
+import AppLayout from "./layouts/AppLayout";
+import Landing from "./pages/landing";
+import Dashboard from "./pages/Dashboard";
+import Auth from "./pages/Auth";
+import Link from "./pages/Link";
+import Redirect from "./pages/Redirect";
+import UrlProvider from "./Context";
+import RequireAuth from "./components/RequireAuth";
+
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Landing />,
+      },
+      {
+        path: "/dashboard",
+        element: (
+          <RequireAuth>
+            <Dashboard />
+          </RequireAuth>
+        ),
+      },
+      {
+        path: "/auth",
+        element: <Auth />,
+      },
+      {
+        path: "/link/:id",
+        element: (
+          <RequireAuth>
+            <Link />
+          </RequireAuth>
+        ),
+      },
+      {
+        path: "/:id",
+        element: <Redirect />,
+      },
+    ],
+  },
+]);
+
+function App() {
+  return (
+    <UrlProvider>
+      <RouterProvider router={router} />
+    </UrlProvider>
+  );
+}
+
+export default App;
